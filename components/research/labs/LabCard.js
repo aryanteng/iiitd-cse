b39fdea2937e07b01615e420db200d882a803df4
aryanteng <aryanteng892@gmail.com>
2024-01-29 00:49:58 +0530

    minor css changes

--- components/research/labs/LabCard.js
+++ components/research/labs/LabCard.js
@@ -6,14 +6,14 @@ export default function LabCard() {
       href="https://sites.google.com/view/vinayakabrol/cross-caps-laboratory"
       target="_blank"
       rel="noopener noreferrer">
-      <div className="w-full shadow-md hover:shadow-xl bg-white border-solid border-1 rounded-lg cursor-pointer duration-500 overflow-hidden pb-2">
+      <div className="w-full shadow-md hover:shadow-xl bg-white border-solid cursor-pointer duration-500 overflow-hidden">
         <img
           src="/images/labs/cross-caps-lab.png"
-          className="object-cover border-b-1 w-full relative"
+          className="object-cover border-b-1 w-full relative aspect-w-16 aspect-h-9"
           loading="lazy"
           alt="Lab Logo"
         />
-        <div className="flex flex-col gap-2 p-2">
+        <div className="flex flex-col gap-2 px-2 py-4">
           <div className="text-primary-dark font-semibold body-large">
             Cross Caps Lab
           </div>
