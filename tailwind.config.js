03f6a2aa74d39a566582dec7203258ee63a471a3
aryanteng <aryanteng892@gmail.com>
2024-01-28 19:25:54 +0530

    tailwind.config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,36 @@
+import { default as themeConstants } from './theme/themeConstants';
+import { screens as _screens } from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        heading: themeConstants.font.heading,
+        body: themeConstants.font.body,
+        default: themeConstants.font.body,
+      },
+      colors: {
+        gg: themeConstants.background.default,
+        paper: themeConstants.background.paper,
+        primary: themeConstants.primary,
+        secondary: themeConstants.secondary,
+        error: themeConstants.error,
+        fg: themeConstants.fg.main,
+        action: '#EE4444',
+        danger: '#EE4444',
+        grey: themeConstants.primary.grey,
+        darkgrey: themeConstants.primary.darkgrey,
+      },
+      screens: {
+        ..._screens,
+        ...themeConstants.breakpoints,
+        ha: { raw: '(hover: hover)' },
       },
     },
   },
