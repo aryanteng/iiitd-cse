1635bce8e05f12e2a02fcb72edb4e6a80acfc6c6
aryanteng <aryanteng892@gmail.com>
2024-01-30 00:47:39 +0530

    created use bottom bar links hook

--- components/layout/header/CustomDrawer.js
+++ components/layout/header/CustomDrawer.js
@@ -8,19 +8,18 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
+import useBottomBarLinks from './links/BottomBarLinks';
 
 export default function CustomDrawer({ open, anchor, toggleDrawer }) {
+  const links = useBottomBarLinks();
   return (
     <Drawer anchor={anchor} open={open} onClose={toggleDrawer}>
       <Box sx={{ width: 250 }} role="presentation" onKeyDown={toggleDrawer}>
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding>
+          {links.map((link, index) => (
+            <ListItem key={link.name} disablePadding>
               <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
+                <ListItemText primary={link.name} />
               </ListItemButton>
             </ListItem>
           ))}
---
+++ components/layout/header/links/BottomBarLinks.js
@@ -0,0 +1,120 @@
+import { useMemo } from 'react';
+
+export default function useBottomBarLinks() {
+  const links = useMemo(
+    () => [
+      {
+        name: 'Academics',
+        menuItems: [
+          {
+            name: 'BTech (CSE) Program',
+            link: 'https://www.iiitd.ac.in/academics/btech/cse',
+          },
+          {
+            name: 'BTech (CSAI) Program',
+            link: 'https://www.iiitd.ac.in/academics/btech/csai',
+          },
+          {
+            name: 'MTech Program',
+            link: 'https://www.iiitd.ac.in/academics/mtech/cse',
+          },
+          {
+            name: 'PhD Program',
+            link: 'https://www.iiitd.ac.in/academics/phd',
+          },
+          {
+            name: 'Guidelines',
+            link: 'https://www.iiitd.ac.in/academics/resources',
+          },
+          { name: 'Approved Online Courses', link: '/approved-online-courses' },
+        ],
+      },
+      {
+        name: 'Research',
+        menuItems: [
+          {
+            name: 'Labs',
+            link: '/research/labs',
+          },
+          {
+            name: 'Themes',
+            link: '/research/themes',
+          },
+          {
+            name: 'Publications',
+            link: '/publications',
+          },
+          {
+            name: 'Projects',
+            link: '/research/projects',
+          },
+        ],
+      },
+      {
+        name: 'People',
+        menuItems: [
+          {
+            name: 'Faculty',
+            link: '/people/faculty',
+          },
+          {
+            name: 'Teaching Fellows',
+            link: '/people/teaching-fellows',
+          },
+          {
+            name: 'Staff',
+            link: '/people/staff',
+          },
+          {
+            name: 'B.Tech. Students',
+            link: '/people/btech',
+          },
+          {
+            name: 'M.Tech. Students',
+            link: '/people/mtech',
+          },
+          {
+            name: 'Ph.D. Students',
+            link: '/people/phd',
+          },
+        ],
+      },
+      {
+        name: 'News & Events',
+        menuItems: [
+          {
+            name: 'Placements',
+            link: '/placements',
+          },
+          {
+            name: 'Media Coverage',
+            link: '/news-and-events/media-coverage',
+          },
+          {
+            name: 'Events',
+            link: '/news-and-events/events',
+          },
+        ],
+      },
+      {
+        name: 'Admissions',
+        menuItems: [
+          {
+            name: 'B.Tech. Admissions',
+            link: 'https://www.iiitd.ac.in/admission/btech/2021',
+          },
+          {
+            name: 'M.Tech. Admissions',
+            link: 'https://www.iiitd.ac.in/admission/mtech/2021',
+          },
+          {
+            name: 'Ph.D. Admissions',
+            link: 'https://www.iiitd.ac.in/admission/phd/2021',
+          },
+        ],
+      },
+    ],
+    [],
+  );
+  return links;
+}
