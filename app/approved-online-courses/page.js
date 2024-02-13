c846dff605dd7969196b74109b3d1ea5533c5512
ritsiiitd <ritick20460@iiitd.ac.in>
2024-02-13 23:14:36 +0530

    added online course directory and page, set up basic boiler plate and course card component

---
+++ app/approved-online-courses/page.js
@@ -0,0 +1,53 @@
+import OnlineCourseCard from '@/components/approved-online-courses/OnlineCourseCard';
+
+export default function ApprovedOnlineCourses() {
+  // Example courses data
+  const courses = [
+    { id: 1, title: 'Course 2', description: 'mollit anim id est laborum.' },
+    {
+      id: 2,
+      title: 'Course 2',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+      id: 3,
+      title: 'Course 2',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+      id: 4,
+      title: 'Course 2',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+      id: 5,
+      title: 'Course 2',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+      id: 6,
+      title: 'Course 2',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    // Add more courses as needed
+  ];
+
+  return (
+    <div>
+      <h1 className="text-[25px] ml-[40px] text-center mt-[50px] font-semibold">
+        Approved Online Courses
+      </h1>
+
+      <div className="mt-4 grid grid-cols-2 mx-auto py-4 lg:py-5 gap-2 sm:gap-4 lg:gap-6 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 w-11/12 max-w-screen-2xl">
+        {courses.map((course) => (
+          <>{course.title}</>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ components/approved-online-courses/OnlineCourseCard.js
@@ -0,0 +1,5 @@
+const OnlineCourseCard = () => {
+  return <div>OnlineCourseCard</div>;
+};
+
+export default OnlineCourseCard;
