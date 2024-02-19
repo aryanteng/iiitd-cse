a5d451401d05d1ec758eb967010f7842a1074e64
Aryan Gupta <96881807+aryanGupta-09@users.noreply.github.com>
2024-02-19 18:57:30 +0530

    added sticky headers to course catalog

--- components/academics/course-catalog/CourseCatalogTable.js
+++ components/academics/course-catalog/CourseCatalogTable.js
@@ -4,8 +4,144 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const rows = [
+  createData(
+    'CSE320',
+    'Advanced Algorithms',
+    'AA',
+    4,
+    'Monsoon',
+    'CSE102',
+    'None',
+  ),
+  createData(
+    'CSE520',
+    'Advanced Biometrics',
+    'BIOM',
+    4,
+    'Winter',
+    'CSE441',
+    'None',
+  ),
+  createData(
+    'CSE562',
+    'Advanced Computer Vision',
+    'ACV',
+    4,
+    'Monsoon',
+    'CSE344',
+    'None',
+  ),
+  createData(
+    'CSE577',
+    'Advanced Internet of Things',
+    'AIOT',
+    4,
+    'Winter',
+    'None',
+    'None',
+  ),
+  createData(
+    'CSE642',
+    'Advanced Machine Learning',
+    'AML',
+    4,
+    'Winter',
+    'MTH201, CSE343',
+    'None',
+  ),
+  createData(
+    'CSE320',
+    'Advanced Algorithms',
+    'AA',
+    4,
+    'Monsoon',
+    'CSE102',
+    'None',
+  ),
+  createData(
+    'CSE520',
+    'Advanced Biometrics',
+    'BIOM',
+    4,
+    'Winter',
+    'CSE441',
+    'None',
+  ),
+  createData(
+    'CSE562',
+    'Advanced Computer Vision',
+    'ACV',
+    4,
+    'Monsoon',
+    'CSE344',
+    'None',
+  ),
+  createData(
+    'CSE577',
+    'Advanced Internet of Things',
+    'AIOT',
+    4,
+    'Winter',
+    'None',
+    'None',
+  ),
+  createData(
+    'CSE642',
+    'Advanced Machine Learning',
+    'AML',
+    4,
+    'Winter',
+    'MTH201, CSE343',
+    'None',
+  ),
+  createData(
+    'CSE320',
+    'Advanced Algorithms',
+    'AA',
+    4,
+    'Monsoon',
+    'CSE102',
+    'None',
+  ),
+  createData(
+    'CSE520',
+    'Advanced Biometrics',
+    'BIOM',
+    4,
+    'Winter',
+    'CSE441',
+    'None',
+  ),
+  createData(
+    'CSE562',
+    'Advanced Computer Vision',
+    'ACV',
+    4,
+    'Monsoon',
+    'CSE344',
+    'None',
+  ),
+  createData(
+    'CSE577',
+    'Advanced Internet of Things',
+    'AIOT',
+    4,
+    'Winter',
+    'None',
+    'None',
+  ),
+  createData(
+    'CSE642',
+    'Advanced Machine Learning',
+    'AML',
+    4,
+    'Winter',
+    'MTH201, CSE343',
+    'None',
+  ),
   createData(
     'CSE320',
     'Advanced Algorithms',
@@ -165,7 +301,9 @@ function createData(
 
 const HeaderCell = ({ children }) => {
   return (
-    <TableCell className="body-small font-semibold border border-black text-white">
+    <TableCell
+      className="body-small font-semibold border border-black text-white"
+      style={{ background: 'var(--primary-color)' }}>
       {children}
     </TableCell>
   );
@@ -200,31 +338,34 @@ const headers = [
 
 export default function BasicTable() {
   return (
-    <TableContainer>
-      <Table
-        className="width-layout-1 border border-black"
-        aria-label="simple table">
-        <TableHead>
-          <TableRow style={{ background: 'var(--primary-color)' }}>
-            {headers.map((header) => (
-              <HeaderCell key={header}>{header}</HeaderCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row, index) => (
-            <TableRow
-              key={row.name}
-              style={{ background: index % 2 ? '#e9e9e9' : 'white' }}>
-              {properties.map((property) => (
-                <ModifiedTableCell key={property}>
-                  {row[property]}
-                </ModifiedTableCell>
+    <Paper elevation={0} sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: '100vh' }}>
+        <Table
+          stickyHeader
+          className="width-layout-1 border border-black"
+          aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {headers.map((header) => (
+                <HeaderCell key={header}>{header}</HeaderCell>
               ))}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row, index) => (
+              <TableRow
+                key={row.name}
+                style={{ background: index % 2 ? '#e9e9e9' : 'white' }}>
+                {properties.map((property) => (
+                  <ModifiedTableCell key={property}>
+                    {row[property]}
+                  </ModifiedTableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
   );
 }
