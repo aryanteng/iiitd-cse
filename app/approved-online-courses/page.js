43ce68b0e7db48610e8dabc4a62a6777716e1848
Ritick Chaudhary <88946197+ritsiiitd@users.noreply.github.com>
2024-02-19 20:41:25 +0530

    Ritick/approved online courses (#1)
    * added online course directory and page, set up basic boiler plate and course card component
    
    * adding course card on main online course page
    
    * online course card designed image container
    
    * online course card designed overlayed course title
    
    * online course card designed overlayed nptel button
    
    * online course card designed description area and faculty name
    
    * online course card designed start button
    
    * online course card designed course time and registration icons
    
    * online course card designed course time and registration icons
    
    * adding courses termiwise
    
    * adding term collapse button
    
    * adding collapse capsule functionality
    
    * conflicts resolved
    
    * padding bugs fixed, card similar to figma design
    
    * Heading text font and color changed

---
+++ app/approved-online-courses/page.js
@@ -0,0 +1,95 @@
+'use client';
+import OnlineCourseCard from '@/components/approved-online-courses/OnlineCourseCard';
+import { Button } from '@mui/material';
+import React from 'react';
+
+export default function ApprovedOnlineCourses() {
+  const [courses, setCourses] = React.useState([
+    {
+      id: 'w23',
+      hidden: false,
+      term: 'winter 2023',
+      courseList: [
+        {
+          id: 1,
+          title: 'Course 2',
+          description: 'mollit anim id est laborum.',
+        },
+        {
+          id: 2,
+          title: 'Course 2',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        },
+        {
+          id: 3,
+          title: 'Course 2',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        },
+        {
+          id: 4,
+          title: 'Course 2',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        },
+        {
+          id: 5,
+          title: 'Course 2',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        },
+        {
+          id: 6,
+          title: 'Course 2',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        },
+      ],
+    },
+  ]);
+
+  const toggleTerm = (termId) => {
+    setCourses((prevCourses) =>
+      prevCourses.map((term) =>
+        term.id === termId ? { ...term, hidden: !term.hidden } : term,
+      ),
+    );
+  };
+
+  return (
+    <div>
+      <div className="body-2xlarge font-semibold text-primary-dark text-center p-4">
+        Approved Online Courses
+      </div>
+
+      <div>
+        {courses.map((term) => (
+          <div className="padding-layout-2" key={term.id}>
+            <div className="w-11/12 max-w-screen-2xl mx-auto">
+              <Button
+                variant="contained"
+                onClick={() => {
+                  toggleTerm(term.id);
+                }}
+                className="bg-primary-dark text-white font-semibold text-sm">
+                {term.term}
+              </Button>
+            </div>
+            {!term.hidden && (
+              <div className="padding-layout-2 grid grid-cols-2 mx-auto gap-2 sm:gap-4 lg:gap-6 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 w-11/12 max-w-screen-2xl">
+                {term?.courseList.map((course) => (
+                  <OnlineCourseCard
+                    key={course.id}
+                    title={course.title}
+                    description={course.description}
+                  />
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
