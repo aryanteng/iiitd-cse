3b9be8c1e4b16536e0d3511e789c7e37d6edc7f0
ritsiiitd <ritick20460@iiitd.ac.in>
2024-02-24 23:31:18 +0530

    added page dir

---
+++ app/admissions/phd/page.js
@@ -0,0 +1,67 @@
+'use client';
+import OnlineCourseCard from '@/components/approved-online-courses/OnlineCourseCard';
+import { Button } from '@mui/material';
+import React from 'react';
+
+export default function ApprovedOnlineCourses() {
+  const [courses, setCourses] = React.useState([
+    {
+      id: 'w23',
+      hidden: false,
+      term: 'winter 2023',
+      courseList: [
+        {
+          id: 1,
+          title: 'Course 2',
+          description: 'mollit anim id est laborum.',
+        },
+        {
+          id: 2,
+          title: 'Course 2',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        },
+        {
+          id: 3,
+          title: 'Course 2',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        },
+        {
+          id: 4,
+          title: 'Course 2',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        },
+        {
+          id: 5,
+          title: 'Course 2',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        },
+        {
+          id: 6,
+          title: 'Course 2',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        },
+      ],
+    },
+  ]);
+
+  const toggleTerm = (termId) => {
+    setCourses((prevCourses) =>
+      prevCourses.map((term) =>
+        term.id === termId ? { ...term, hidden: !term.hidden } : term,
+      ),
+    );
+  };
+
+  return (
+    <div>
+      <div className="body-2xlarge font-semibold text-primary-dark text-center p-4">
+        Approved Online Courses
+      </div>
+    </div>
+  );
+}
