f13b2fb06a5a1997987d30e8d94dcdd8a5383b11
ritsiiitd <ritick20460@iiitd.ac.in>
2024-03-03 23:57:17 +0530

    fixed carousel lag

--- components/admissions/phd/Carousell.js
+++ components/admissions/phd/Carousell.js
@@ -1,77 +1,16 @@
-import 'flowbite-react';
+'use client';
+
+import { Carousel } from 'flowbite-react';
 export default function Carousell() {
   return (
-    <div id="default-carousel" class="relative w-full" data-carousel="slide">
-      <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-        <div className="duration-700 ease-in-out" data-carousel-item>
-          <img
-            src="/images/admissions/phd/carousel1.jpg"
-            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="image 1"
-          />
-        </div>
-
-        <div className="duration-700 ease-in-out" data-carousel-item="active">
-          <img
-            src="/images/admissions/phd/carousel2.jpg"
-            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="image 2"
-          />
-        </div>
-
-        <div className="duration-700 ease-in-out" data-carousel-item>
-          <img
-            src="/images/admissions/phd/carousel3.jpg"
-            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="image 3"
-          />
-        </div>
-      </div>
-
-      <button
-        type="button"
-        className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        data-carousel-prev>
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg
-            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10">
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M5 1 1 5l4 4"
-            />
-          </svg>
-          <span className="sr-only">Previous</span>
-        </span>
-      </button>
-      <button
-        type="button"
-        className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        data-carousel-next>
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg
-            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10">
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m1 9 4-4-4-4"
-            />
-          </svg>
-          <span className="sr-only">Next</span>
-        </span>
-      </button>
+    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+      <Carousel>
+        <img src="/images/admissions/phd/carousel3.jpg" alt="..." />
+        <img src="/images/admissions/phd/carousel2.jpg" alt="..." />
+        <img src="/images/admissions/phd/carousel1.jpg" alt="..." />
+        {/* <img src="/images/admissions/phd/carousel4.jpg" alt="..." /> */}
+        {/* <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." /> */}
+      </Carousel>
     </div>
   );
 }
