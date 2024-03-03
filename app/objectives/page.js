0c6f2d32d82ddd40cafc17c3ce6482a2ccca0e76
Yug1110 <yugayu21506@iiitd.ac.in>
2024-03-03 23:38:17 +0530

    completed objectives page

---
+++ app/objectives/page.js
@@ -0,0 +1,367 @@
+export default function page() {
+  return (
+    <main className="mx-4 md:mx-20 md:pb-20 pb-10">
+      <section>
+        <h1 className="heading-2 padding-layout-2 text-primary-dark">
+          Program Educational Objectives (PEOs) [BTech]
+        </h1>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            To undertake industry careers involving innovation and problem
+            solving using software and other information technologies.
+          </p>
+        </div>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            To undertake advanced studies for research careers in different
+            areas of computer sciences and allied areas.
+          </p>
+        </div>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            To undertake entrepreneurship as a career by creating products and
+            services using technology to provide value to some sections of the
+            society.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h1 className="heading-2 padding-layout-2 text-primary-dark">
+          Program Outcomes (POs) [BTech]
+        </h1>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            <span className="font-semibold">Engineering knowledge:</span> Apply
+            the knowledge of mathematics, science, engineering fundamentals and
+            an engineering specialization to the solution of complex engineering
+            problems.
+          </p>
+        </div>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            <span className="font-semibold">Problem analysis:</span> Identify,
+            formulate, review research literature, and analyze complex
+            engineering problems reaching substantiated conclusions using first
+            principles of mathematics, natural sciences, and engineering
+            sciences.
+          </p>
+        </div>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            <span className="font-semibold">
+              Design/development of solutions:{' '}
+            </span>
+            Design solutions for complex engineering problems and design system
+            components or processes that meet the specified needs with
+            appropriate consideration for the public health and safety, and the
+            cultural, societal, and environmental considerations.
+          </p>
+        </div>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            <span className="font-semibold">
+              Conduct investigations of complex problems:{' '}
+            </span>
+            Use research-based knowledge and research methods including design
+            of experiments, analysis and interpretation of data, and synthesis
+            of the information to provide valid conclusions.
+          </p>
+        </div>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            <span className="font-semibold">Modern tool usage: </span>
+            Create, select, and apply appropriate techniques, resources, and
+            modern engineering and IT tools including prediction and modeling to
+            complex engineering activities with an understanding of the
+            limitations.
+          </p>
+        </div>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            <span className="font-semibold">The engineer and society: </span>
+            Apply reasoning informed by the contextual knowledge to assess
+            societal, health, safety, legal and cultural issues and the
+            consequent responsibilities relevant to the professional engineering
+            practice.
+          </p>
+        </div>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            <span className="font-semibold">
+              Environment and sustainability:{' '}
+            </span>
+            Understand the impact of the professional engineering solutions in
+            societal and environmental contexts, and demonstrate the knowledge
+            of, and need for sustainable development.
+          </p>
+        </div>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            <span className="font-semibold">Ethics: </span>
+            Apply ethical principles and commit to professional ethics and
+            responsibilities and norms of the engineering practice.
+          </p>
+        </div>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            <span className="font-semibold">Individual and team work: </span>
+            Function effectively as an individual, and as a member or leader in
+            diverse teams, and in multidisciplinary settings.
+          </p>
+        </div>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            <span className="font-semibold">Communication: </span>
+            Communicate effectively on complex engineering activities with the
+            engineering community and with society at large, such as, being able
+            to comprehend and write effective reports and design documentation,
+            make effective presentations, and give and receive clear
+            instructions.
+          </p>
+        </div>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            <span className="font-semibold">
+              Project management and finance:{' '}
+            </span>
+            Demonstrate knowledge and understanding of the engineering and
+            management principles and apply these to one’s own work, as a member
+            and leader in a team, to manage projects and in multidisciplinary
+            environments.
+          </p>
+        </div>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            <span className="font-semibold">Life-long learning: </span>
+            Recognize the need for, and have the preparation and ability to
+            engage in independent and life-long learning in the broadest context
+            of technological change.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h1 className="heading-2 padding-layout-2 text-primary-dark">
+          Program Specific Outcomes (PSOs) [BTech (CSE)]
+        </h1>
+        <p className="body-small mb-2">
+          The outcomes specific to the BTech (CSE) program are given here. At
+          the end of the program, a student should have:
+        </p>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            Understanding of foundational topics in Computer Science, Artificial
+            Intelligence, and Machine Learning.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            Understanding of theoretical foundations and limits of Artificial
+            Intelligence and Machine Learning.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            Ability to design and implement algorithms and data structures for
+            efficiently solving new problems.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            Ability to model and analyze a variety of problems using appropriate
+            mathematical/computational and AI concepts.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            Ability of apply and develop AI algorithms to transform large amount
+            of data into intelligent decisions and/or behavior.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            An understanding of the impact of AI based solutions in an economic,
+            societal, and environment context.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h1 className="heading-2 padding-layout-2 text-primary-dark">
+          Program Specific Outcomes (PSOs) [BTech (CSAI)]
+        </h1>
+        <p className="body-small mb-2">
+          The outcomes specific to the BTech (CSAI) program are given here. At
+          the end of this program, a student should have:
+        </p>
+
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            Understanding of foundational topics in Computer Science, Artificial
+            Intelligence, and Machine Learning.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            Understanding of theoretical foundations and limits of Artificial
+            Intelligence and Machine Learning.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            Ability to model and analyze a variety of problems using appropriate
+            mathematical/computational and AI concepts.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            Ability of apply and develop AI algorithms to transform large amount
+            of data into intelligent decisions and/or behavior.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            An understanding of the impact of AI based solutions in an economic,
+            societal, and environment context.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h1 className="heading-2 padding-layout-2 text-primary-dark">
+          Program Educational Objectives (PEOs) [MTech (CSE)]
+        </h1>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            To undertake industry careers involving innovation and problem
+            solving using software and other information technologies.
+          </p>
+        </div>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            To undertake research careers in Computer Sciences and allied areas.
+          </p>
+        </div>
+        <div className="points">
+          <div className="bullet-point" />
+          <p>
+            To contribute to society by becoming a model professional who can
+            communicate effectively and observes ethical behavior.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h1 className="heading-2 padding-layout-2 text-primary-dark">
+          Program Outcomes (POs) [MTech (CSE)]
+        </h1>
+        <p className="body-small mb-2">
+          The main outcomes of the MTech (CSE) program are given here. At the
+          end of the program, a student should have:
+        </p>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            An understanding of the theoretical foundations and the limits of
+            computing.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            An ability to adapt existing models, techniques, algorithms, data
+            structures, etc. for efficiently solving problems.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            An ability to design, develop and evaluate new computer based
+            systems for novel applications which meet the desired needs of
+            industry and society.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            Understanding and ability to use advanced computing techniques and
+            tools.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            An ability to undertake original research at the cutting edge of
+            computer science & its related areas.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            An ability to function effectively individually or as a part of a
+            team to accomplish a state goal.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            An ability to function effectively individually or as a part of a
+            team to accomplish a state goal.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>An understanding of professional and ethical responsibility.</p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            An ability to communicate effectively with a wide range of audience.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            An ability to learn independently and engage in lifelong learning.
+          </p>
+        </div>
+        <div className="points-indent">
+          <div className="bullet-point" />
+          <p>
+            An understanding of the impact of IT related solutions in an
+            economic, societal, and environment context.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+}
