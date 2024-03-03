624dc46251457645925ec1ce8583f2c38d39777f
Aryan Gupta <96881807+aryanGupta-09@users.noreply.github.com>
2024-03-03 20:16:11 +0530

    changed size and color of course catalog heading

--- app/academics/course-catalog/page.js
+++ app/academics/course-catalog/page.js
@@ -3,7 +3,9 @@ import CourseCatalogTable from '@/components/academics/course-catalog/CourseCata
 export default function CourseCatalog() {
   return (
     <div className="padding-layout-2 flex flex-col items-center justify-center gap-4">
-      <h2 className="title">Course Catalog</h2>
+      <h2 className="body-2xlarge" style={{ color: 'var(--primary-dark)' }}>
+        Course Catalog
+      </h2>
       <CourseCatalogTable />
     </div>
   );
