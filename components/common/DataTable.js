d11f13d5037fa711304d89246d7ee79e709a6c50
aryanteng <aryanteng892@gmail.com>
2024-03-04 02:15:29 +0530

    fixed issues

--- components/common/DataTable.js
+++ components/common/DataTable.js
@@ -13,14 +13,18 @@ function DataTable({ data, columns, initialRows }) {
           MuiInputBase: {
             styleOverrides: {
               input: {
-                color: 'white',
+                color: 'white', // Keeps the search input text white
+                '&.Mui-focused': {
+                  color: 'black', // Ensures the text stays black when input is focused
+                },
               },
-            },
-          },
-          MUIDataTable: {
-            styleOverrides: {
-              paper: {
-                margin: 'auto',
+              root: {
+                '&.MuiInput-underline:after': {
+                  borderBottomColor: 'white', // Changes the underline color when input is focused
+                },
+                '&.MuiInput-underline:before': {
+                  borderBottom: '1px solid white', // Changes the underline to white
+                },
               },
             },
           },
@@ -56,10 +60,54 @@ function DataTable({ data, columns, initialRows }) {
               },
             },
           },
+          MUIDataTablePagination: {
+            styleOverrides: {
+              selectIcon: {
+                color: 'black', // Makes the dropdown icon black
+              },
+              select: {
+                color: 'black', // Makes the rows per page text black
+                '&.Mui-focused': {
+                  color: 'black', // Ensures the text stays black when dropdown is focused
+                  backgroundColor: 'white', // Changes background to white when dropdown is focused
+                },
+              },
+            },
+          },
+          MuiSvgIcon: {
+            styleOverrides: {
+              root: {
+                color: 'white', // Changes default color for icons to white
+              },
+            },
+          },
+          // Add overrides for the MuiSelect component used within the pagination
+          MuiSelect: {
+            styleOverrides: {
+              select: {
+                color: 'black', // Text color for the select
+                '&.Mui-focused': {
+                  backgroundColor: 'white', // Background color when select is focused
+                },
+              },
+              icon: {
+                color: 'black', // Dropdown icon color
+              },
+            },
+          },
+          // Add overrides for the search icon
+          MuiIconButton: {
+            styleOverrides: {
+              root: {
+                color: 'white', // Changes the color of icon buttons to white
+              },
+            },
+          },
         },
       }),
     [],
   );
+
   return (
     <ThemeProvider theme={getMuiTheme()}>
       <MUIDataTable
