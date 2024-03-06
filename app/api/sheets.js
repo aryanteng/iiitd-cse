1f7b2a24e8e623bb4109c64b0ac68fdb8c3671d9
aryanteng <aryanteng892@gmail.com>
2024-03-07 00:41:14 +0530

    split method

--- app/api/sheets.js
+++ app/api/sheets.js
@@ -9,7 +9,9 @@ export async function getSheetData(spreadsheetId, range) {
         type: 'service_account',
         project_id: process.env.GOOGLE_SHEETS_PROJECT_ID,
         private_key_id: process.env.GOOGLE_SHEETS_PRIVATE_KEY_ID,
-        private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+        private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.split(
+          String.raw`\n`,
+        ).join('\n'),
         client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
         client_id: process.env.GOOGLE_SHEETS_CLIENT_ID,
         auth_uri: process.env.GOOGLE_AUTH_URI,
