e8ceadc2233b0b894a65845ffcc5f5629ae64b0c
aryanteng <aryanteng892@gmail.com>
2024-03-07 00:45:49 +0530

    replace method

--- app/api/sheets.js
+++ app/api/sheets.js
@@ -9,9 +9,10 @@ export async function getSheetData(spreadsheetId, range) {
         type: 'service_account',
         project_id: process.env.GOOGLE_SHEETS_PROJECT_ID,
         private_key_id: process.env.GOOGLE_SHEETS_PRIVATE_KEY_ID,
-        private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.split(
-          String.raw`\n`,
-        ).join('\n'),
+        private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(
+          /\\n/gm,
+          '\n',
+        ),
         client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
         client_id: process.env.GOOGLE_SHEETS_CLIENT_ID,
         auth_uri: process.env.GOOGLE_AUTH_URI,
