61d5a22413ac26c3716e430bbb5521db27e66916
aryanteng <aryanteng892@gmail.com>
2024-03-07 00:38:32 +0530

    removed replave method

--- app/api/sheets.js
+++ app/api/sheets.js
@@ -9,10 +9,7 @@ export async function getSheetData(spreadsheetId, range) {
         type: 'service_account',
         project_id: process.env.GOOGLE_SHEETS_PROJECT_ID,
         private_key_id: process.env.GOOGLE_SHEETS_PRIVATE_KEY_ID,
-        private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(
-          /\\n/g,
-          '\n',
-        ),
+        private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
         client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
         client_id: process.env.GOOGLE_SHEETS_CLIENT_ID,
         auth_uri: process.env.GOOGLE_AUTH_URI,
