e28369abc0bebf4699acb777021eec168e19cfdb
aryanteng <aryanteng892@gmail.com>
2024-03-07 01:22:49 +0530

    entire credentials to base64

--- app/api/sheets.js
+++ app/api/sheets.js
@@ -2,27 +2,17 @@ import { google } from 'googleapis';
 
 const sheets = google.sheets({ version: 'v4' });
 
-const googleSheetsPrivateKey = process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(
-  /\\n/g,
-  '\n',
-);
+const credentialsJsonString = Buffer.from(
+  process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64,
+  'base64',
+).toString('utf-8');
+
+const credentials = JSON.parse(credentialsJsonString);
 
 export async function getSheetData(spreadsheetId, range) {
   try {
     const auth = new google.auth.GoogleAuth({
-      credentials: {
-        type: 'service_account',
-        project_id: process.env.GOOGLE_SHEETS_PROJECT_ID,
-        private_key_id: process.env.GOOGLE_SHEETS_PRIVATE_KEY_ID,
-        private_key: googleSheetsPrivateKey,
-        client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-        client_id: process.env.GOOGLE_SHEETS_CLIENT_ID,
-        auth_uri: process.env.GOOGLE_AUTH_URI,
-        token_uri: process.env.GOOGLE_TOKEN_URI,
-        auth_provider_x509_cert_url:
-          process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
-        client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
-      },
+      credentials: credentials,
       scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
     });
 
