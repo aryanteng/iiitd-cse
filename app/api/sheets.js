c301f63303d6e48e0a9f66c1daf9401d65223256
aryanteng <aryanteng892@gmail.com>
2024-03-07 01:05:19 +0530

    try to fix build

--- app/api/sheets.js
+++ app/api/sheets.js
@@ -2,6 +2,11 @@ import { google } from 'googleapis';
 
 const sheets = google.sheets({ version: 'v4' });
 
+const googleSheetsPrivateKey = process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(
+  /\\n/g,
+  '\n',
+);
+
 export async function getSheetData(spreadsheetId, range) {
   try {
     const auth = new google.auth.GoogleAuth({
@@ -9,10 +14,7 @@ export async function getSheetData(spreadsheetId, range) {
         type: 'service_account',
         project_id: process.env.GOOGLE_SHEETS_PROJECT_ID,
         private_key_id: process.env.GOOGLE_SHEETS_PRIVATE_KEY_ID,
-        private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY.replace(
-          /\\n/gm,
-          '\n',
-        ),
+        private_key: googleSheetsPrivateKey,
         client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
         client_id: process.env.GOOGLE_SHEETS_CLIENT_ID,
         auth_uri: process.env.GOOGLE_AUTH_URI,
