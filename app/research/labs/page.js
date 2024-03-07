e62d8609f5922907aa712f4564768496e5dd398f
aryanteng <aryanteng892@gmail.com>
2024-03-07 19:37:00 +0530

    labs from sheets integrated, wrote a normalising function to dynamically process data

--- app/research/labs/page.js
+++ app/research/labs/page.js
@@ -1,10 +1,19 @@
+import { getSheetData } from '@/app/api/sheets';
 import LabCard from '@/components/research/labs/LabCard';
+import { normalizeSheetData } from '@/utils/normaliseSheetData';
 
-export default function Labs() {
+export default async function Labs() {
+  const spreadsheetId = '1cYPbswNr4ZvvQhzkfIOaM9Q75cSDP1CSl_JELvmA4iQ';
+  const range = 'Labs';
+
+  const data = await getSheetData(spreadsheetId, range);
+  const labs = normalizeSheetData(data);
+  console.log(labs);
+
   return (
     <div className="grid grid-cols-2 mx-auto py-4 lg:py-5 gap-2 sm:gap-4 lg:gap-5 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 w-11/12 max-w-screen-2xl">
-      {Array.from({ length: 40 }).map((lab, index) => (
-        <LabCard key={index} />
+      {labs.map((lab, index) => (
+        <LabCard key={index} {...lab} />
       ))}
     </div>
   );
