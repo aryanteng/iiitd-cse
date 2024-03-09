44d6c2be14d9eb1313c293c99ecd39358b6748d0
Aditya Ahuja <88674120+adityaahuja7@users.noreply.github.com>
2024-03-09 14:20:38 +0530

    Adityaahuja/btech datafetch (#15)
    * Fixed student tables styling. Fetch content from backend.
    
    * Remove bugs
    
    * Added Mtech/Btech DataTable wrapper components.
    
    * Minor bug fixes in data display.
    
    * Removed redundant options prop.

---
+++ components/people/btech/BtechDataTable.js
@@ -0,0 +1,59 @@
+'use client';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import DataTable from '@/components/common/DataTable';
+import React, { useCallback } from 'react';
+
+export default function BtechDataTable({ data, initialRows }) {
+  const getColumns = useCallback(() => {
+    return [
+      {
+        name: 'Serial Number',
+        options: {
+          customBodyRender: (value) => {
+            return <span className="body-xsmall">{value}</span>;
+          },
+          customHeadLabelRender: (value) => {
+            return <span className="body-small">{value.name}</span>;
+          },
+        },
+      },
+      {
+        name: 'Roll Number',
+        options: {
+          customBodyRender: (value) => {
+            return <span className="body-xsmall">{value}</span>;
+          },
+          customHeadLabelRender: (value) => {
+            return <span className="body-small">{value.name}</span>;
+          },
+        },
+      },
+      {
+        name: 'Name',
+        options: {
+          customBodyRender: (value) => {
+            return <span className="body-xsmall">{value}</span>;
+          },
+          customHeadLabelRender: (value) => {
+            return <span className="body-small">{value.name}</span>;
+          },
+        },
+      },
+      {
+        name: 'Joining year',
+        options: {
+          customBodyRender: (value) => {
+            return <span className="body-xsmall">{value}</span>;
+          },
+          customHeadLabelRender: (value) => {
+            return <span className="body-small">{value.name}</span>;
+          },
+        },
+      },
+    ];
+  });
+
+  return (
+    <DataTable data={data} columns={getColumns()} initialRows={initialRows} />
+  );
+}
