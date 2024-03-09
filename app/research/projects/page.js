d785ae295a38c0d878d0cc18cc3983b23d0223f8
aryanteng <aryanteng892@gmail.com>
2024-03-09 19:57:54 +0530

    code fixes, build fixes

--- app/research/projects/page.js
+++ app/research/projects/page.js
@@ -11,7 +11,6 @@ export default async function Projects() {
   projects.forEach((entry) => {
     entry.funded_by = entry.funded_by.split(',').map((item) => item.trim());
   });
-  console.log(projects);
 
   return (
     <div className="grid grid-cols-2 mx-auto py-4 lg:py-5 gap-2 sm:gap-4 lg:gap-5 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 w-11/12 max-w-screen-2xl">
--- components/research/projects/ProjectCard.js
+++ components/research/projects/ProjectCard.js
@@ -1,8 +1,7 @@
-import { AddBox } from '@mui/icons-material';
-import { Chip } from '@mui/material';
+/* eslint-disable @next/next/no-img-element */
+import { Avatar, Chip } from '@mui/material';
 
 export default function ProjectCard({
-  key,
   title,
   faculty,
   faculty_image,
@@ -17,42 +16,44 @@ export default function ProjectCard({
       target="_blank"
       rel="noopener noreferrer"
       className="block w-full h-full">
-      <div className="w-full h-full shadow-md hover:shadow-xl bg-white border-solid cursor-pointer duration-500 overflow-hidden flex flex-col gap-2 px-2 py-4">
+      <div className="w-full h-full shadow-md hover:shadow-xl bg-white border-solid cursor-pointer duration-500 overflow-hidden flex flex-col gap-2 py-2 px-2">
         <div className="text-primary-dark h-2/5 font-semibold body-normal">
           {title}
         </div>
         <div className="text-primary-dark font-semibold text-right body-xsmall">
           {year}
         </div>
-        <div
-          id="faculty"
-          className="text-primary-dark py-2 font-semibold body-normal">
-          <div className="flex gap-2 pt-4 items-center">
-            <img
-              src={faculty_image}
-              className="rounded-full cursor-pointer w-1/6"
-            />
-            <div className="cursor-pointer body-xsmall">{faculty}</div>
-          </div>
-        </div>
-        <div className="body-xsmall py-2 body-small">
-          Funded By :{' '}
-          <div className="flex flex-row gap-2 py-1">
-            {funded_by?.map((org, index) => (
-              <Chip
-                label={org}
-                title={org}
-                className="body-xsmall w-2/6 font-semibold bg-primary-dark text-white text-left overflow-hidden truncate cursor-pointer hover:overflow-visible hover:whitespace-normal"
-              />
-            ))}
-          </div>
-        </div>
-        <div className="mt-auto py-2 body-xsmall">
-          Sanction Amount :{' '}
-          <Chip
-            label={sanction_amount + ' INR'}
-            className="text-xs font-semibold bg-primary-dark text-white text-left"
+        <div className="flex gap-2 items-center pt-2">
+          <Avatar
+            src={faculty_image}
+            alt="Faculty Image"
+            className="w-8 h-8 sm:w-10 sm:h-10"
           />
+          <div className="font-semibold body-xsmall">{faculty}</div>
+        </div>
+        <div className="flex flex-col gap-2 py-2 px-2">
+          <div className="body-xsmall">
+            Funded By :{' '}
+            <div className="flex flex-row gap-2 py-1">
+              {funded_by?.map((org) => (
+                <Chip
+                  key={org}
+                  label={org}
+                  title={org}
+                  className="font-semibold"
+                />
+              ))}
+            </div>
+          </div>
+          <div className="body-xsmall flex items-center gap-2">
+            Sanction Amount:{' '}
+            <div>
+              <Chip
+                label={sanction_amount + ' INR'}
+                className="bg-primary-dark text-white font-semibold"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </a>
