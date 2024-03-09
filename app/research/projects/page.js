1d9c139b947e252cdcb2737a745b50c9ba75eea8
Ritick Chaudhary <88946197+ritsiiitd@users.noreply.github.com>
2024-03-09 10:43:13 +0000

    projects page completed along with backend

--- app/research/projects/page.js
+++ app/research/projects/page.js
@@ -4,76 +4,14 @@ import { normalizeSheetData } from '@/utils/normaliseSheetData';
 
 export default async function Projects() {
   const spreadsheetId = '1cYPbswNr4ZvvQhzkfIOaM9Q75cSDP1CSl_JELvmA4iQ';
-  const range = 'Labs';
+  const range = 'Projects';
 
   const data = await getSheetData(spreadsheetId, range);
-  const labs = normalizeSheetData(data);
-
-  const projects = [
-    {
-      title:
-        'Adaptive VR driving based on physiological signals of the subject.',
-      faculty: 'Dr. Jainendra Shukla',
-      link: '#',
-      year: '2019-20',
-      fundedBy: ['CAI', 'SPARC', 'MHRD'],
-      sanctionAmount: '960000',
-    },
-    {
-      title:
-        'Adaptive VR driving based on physiological signals of the subject.',
-      faculty: 'Dr. Jainendra Shukla',
-      link: '#',
-      year: '2019-20',
-      fundedBy: ['CAI'],
-      sanctionAmount: '960000',
-    },
-    {
-      title:
-        'Adaptive VR driving based on physiological signals of the subject.',
-      faculty: 'Dr. Jainendra Shukla',
-      link: '#',
-      year: '2019-20',
-      fundedBy: ['CAI'],
-      sanctionAmount: '960000',
-    },
-    {
-      title:
-        'Adaptive VR driving based on physiological signals of the subject.',
-      faculty: 'Dr. Jainendra Shukla',
-      link: '#',
-      year: '2019-20',
-      fundedBy: ['CAI'],
-      sanctionAmount: '960000',
-    },
-    {
-      title:
-        'Adaptive VR driving based on physiological signals of the subject.',
-      faculty: 'Dr. Jainendra Shukla',
-      link: '#',
-      year: '2019-20',
-      fundedBy: ['CAIII', 'SPARC', 'MHRD'],
-      sanctionAmount: '960000',
-    },
-    {
-      title:
-        'Adaptive VR driving based on physiological signals of the subject.',
-      faculty: 'Dr. Jainendra Shukla',
-      link: '#',
-      year: '2019-20',
-      fundedBy: ['CAI'],
-      sanctionAmount: '960000',
-    },
-    {
-      title:
-        'Adaptive VR driving based on physiological signals of the subject.',
-      faculty: 'Dr. Jainendra Shukla',
-      link: '#',
-      year: '2019-20',
-      fundedBy: ['CAI', 'SPARC', 'MHRD'],
-      sanctionAmount: '960000',
-    },
-  ];
+  const projects = normalizeSheetData(data);
+  projects.forEach((entry) => {
+    entry.funded_by = entry.funded_by.split(',').map((item) => item.trim());
+  });
+  console.log(projects);
 
   return (
     <div className="grid grid-cols-2 mx-auto py-4 lg:py-5 gap-2 sm:gap-4 lg:gap-5 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 w-11/12 max-w-screen-2xl">
--- components/research/projects/ProjectCard.js
+++ components/research/projects/ProjectCard.js
@@ -1,13 +1,15 @@
-/* eslint-disable @next/next/no-img-element */
-
 import { AddBox } from '@mui/icons-material';
+import { Chip } from '@mui/material';
 
 export default function ProjectCard({
+  key,
   title,
   faculty,
+  faculty_image,
+  funded_by,
+  year,
+  sanction_amount,
   link,
-  fundedBy,
-  sanctionAmount,
 }) {
   return (
     <a
@@ -16,38 +18,41 @@ export default function ProjectCard({
       rel="noopener noreferrer"
       className="block w-full h-full">
       <div className="w-full h-full shadow-md hover:shadow-xl bg-white border-solid cursor-pointer duration-500 overflow-hidden flex flex-col gap-2 px-2 py-4">
-        <div className="text-primary-dark font-semibold body-normal">
+        <div className="text-primary-dark h-2/5 font-semibold body-normal">
           {title}
+        </div>
+        <div className="text-primary-dark font-semibold text-right body-small">
+          {year}
         </div>
         <div
           id="faculty"
           className="text-primary-dark py-2 font-semibold body-normal">
           <div className="flex gap-2 pt-4 items-center">
             <img
-              src="https://iiitd.ac.in/sites/default/files/images/faculty/diptapriyo.jpg"
+              src={faculty_image}
               className="rounded-full cursor-pointer w-1/6"
             />
-            <div className="cursor-pointer body-xsmall">A V Subramanyam</div>
+            <div className="cursor-pointer body-xsmall">{faculty}</div>
           </div>
         </div>
         <div className="body-xsmall py-2 body-small">
           Funded By :{' '}
-          {fundedBy?.map((org, index) => (
-            <span key={org}>
-              <div className="flex flex-row gap-1" key={org}>
-                <div className="">
-                  <AddBox className="text-primary-light h-6 w-6"></AddBox>
-                </div>
-                <div className="body-xsmall text-left pt-1">{org}</div>
-              </div>
-            </span>
-          ))}
+          <div className="flex flex-row gap-2 py-1">
+            {funded_by?.map((org, index) => (
+              <Chip
+                label={org}
+                title={org}
+                className="body-xsmall w-2/6 font-semibold bg-primary-dark text-white text-left overflow-hidden truncate cursor-pointer hover:overflow-visible hover:whitespace-normal"
+              />
+            ))}
+          </div>
         </div>
         <div className="mt-auto py-2 body-xsmall">
           Sanction Amount :{' '}
-          <span className="bg-primary-light text-white px-1">
-            {sanctionAmount} INR
-          </span>
+          <Chip
+            label={sanction_amount + ' INR'}
+            className="body-xsmall font-semibold bg-primary-dark text-white text-left"
+          />
         </div>
       </div>
     </a>
