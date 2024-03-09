6deea2b4ad66cb7be23db40d588e167f79605886
Ritick Chaudhary <88946197+ritsiiitd@users.noreply.github.com>
2024-03-09 23:37:07 +0530

    Ritick/approved online cources/backend (#14)
    * added project setup/routing
    
    * modifying labcard
    
    * projects page complete
    
    * data fetched from spreadsheet, bg images remaining
    
    * fixed toggle button width
    
    * randomized images for card background
    
    * randomized images for card background added one more image
    
    * fixes
    
    ---------
    
    Co-authored-by: aryanteng <aryanteng892@gmail.com>

--- components/approved-online-courses/OnlineCourseCard.js
+++ components/approved-online-courses/OnlineCourseCard.js
@@ -1,27 +1,37 @@
 /* eslint-disable @next/next/no-img-element */
 import HourGlass from '@mui/icons-material/HourglassBottomTwoTone';
 import ScholarHat from '@mui/icons-material/School';
-import { Button } from '@mui/material';
+import { Avatar, Button } from '@mui/material';
 
-function OnlineCourseCard({ title, description }) {
+function OnlineCourseCard({
+  title,
+  link,
+  faculty,
+  description,
+  weeks,
+  registrations,
+  facultyimage,
+  platform,
+}) {
+  const x = Math.floor(Math.random() * 11) + 1;
+  const imageSrc = `/images/approved-online-courses/onlineCourses_${x}.jpg`;
+
   return (
     <div className="shadow-md hover:shadow-xl bg-white border-solid duration-500 overflow-hidden flex flex-col">
       <div className="flex-grow">
         <div className="relative h-auto">
           <img
-            src="/images/approved-online-courses/internetOfThings.jpg"
+            src={imageSrc}
             className="object-cover h-48 border-b-1 w-full opacity-90 brightness-50"
             loading="lazy"
             alt="IOT"
           />
           {/* Overlay Button */}
           <div className="absolute cursor-default bottom-2 right-2 bg-primary-dark text-white py-1 px-3 font-semibold rounded-xl text-sm">
-            NPTEL
+            {platform}
           </div>
           <h1 className="absolute top-1/3 left-2 text-white font-bold text-3xl">
-            Internet of
-            <br />
-            things
+            {title}
           </h1>
         </div>
         <div className="flex flex-col gap-2 px-4 py-2">
@@ -31,24 +41,21 @@ function OnlineCourseCard({ title, description }) {
               id="faculty"
               className="text-primary-dark py-2 font-semibold body-normal">
               <div className="flex gap-2 items-center">
-                <img
-                  src="images/people/faculty.png"
-                  className="rounded-full cursor-pointer w-1/6"
-                />
-                <div className="cursor-pointer text-xs">A V Subramanyam</div>
+                <Avatar src={facultyimage} />
+                <div className="cursor-pointer text-xs">{faculty}</div>
               </div>
             </div>
             <div className="flex gap-2 flex-col">
               <div className="flex flex-row">
                 <HourGlass className="text-primary-dark body-normal" />
                 <span className="text-xs flex items-center whitespace-nowrap">
-                  12 weeks
+                  {weeks} weeks
                 </span>
               </div>
               <div className="flex flex-row">
                 <ScholarHat className="text-primary-dark body-normal" />
                 <span className="text-xs ml-1 flex items-center whitespace-nowrap">
-                  17852
+                  {registrations}
                 </span>
               </div>
             </div>
@@ -57,8 +64,11 @@ function OnlineCourseCard({ title, description }) {
       </div>
       {/* Start Button at the bottom */}
       <Button
+        href={link}
         variant="contained"
-        className="w-full text-center bg-primary-dark text-white font-semibold">
+        target="_blank"
+        rel="noopener noreferrer"
+        className="w-full bg-primary-dark text-white font-semibold">
         Start
       </Button>
     </div>
