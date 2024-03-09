b6151dd723d46815c1f9ae35ffa455b4ece47548
Aryan Gupta <96881807+aryanGupta-09@users.noreply.github.com>
2024-03-09 20:08:36 +0530

    AryanGupta/404-Page (#12)
    * added the 404 page
    
    * reduced size of home button
    
    * changed some text and used a clearer image

---
+++ app/not-found.js
@@ -0,0 +1,31 @@
+import Button from '@mui/material/Button';
+
+export default function NotFound() {
+  return (
+    <div className="relative h-screen">
+      <img
+        src="/images/404-page/background.png"
+        alt="404 background image"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+        <h1 className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-body font-bold mb-2 text-white">
+          404
+        </h1>
+        <h2 className="heading-2 font-semibold" style={{ color: 'white' }}>
+          Page Not Found
+        </h2>
+        <h3 className="body-xlarge font-semibold" style={{ color: 'white' }}>
+          Oops! The page you are looking for does not exist
+        </h3>
+        <Button
+          variant="contained"
+          href="/"
+          className="text-sm md:text-base lg:text-lg xl:text-xl mt-3"
+          style={{ backgroundColor: 'var(--primary-dark)' }}>
+          Return to Home
+        </Button>
+      </div>
+    </div>
+  );
+}
