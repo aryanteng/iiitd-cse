8ba6a4d52c4bba6c7e19c056364f5461290fd575
Ritick Chaudhary <88946197+ritsiiitd@users.noreply.github.com>
2024-03-09 20:07:03 +0530

    Ritick/projects (#13)
    * added project setup/routing
    
    * modifying labcard
    
    * projects page complete
    
    * minor fixes
    
    * added INR
    
    * redesigned project card
    
    * projects page completed along with backend
    
    * conflicts
    
    * making text smaller for large devices on chip
    
    * code fixes, build fixes
    
    ---------
    
    Co-authored-by: aryanteng <aryanteng892@gmail.com>

--- app/research/projects/page.js
+++ app/research/projects/page.js
@@ -1,18 +1,21 @@
 import { getSheetData } from '@/app/api/sheets';
-import LabCard from '@/components/research/labs/LabCard';
+import ProjectCard from '@/components/research/projects/ProjectCard';
 import { normalizeSheetData } from '@/utils/normaliseSheetData';
 
-export default async function Labs() {
+export default async function Projects() {
   const spreadsheetId = '1cYPbswNr4ZvvQhzkfIOaM9Q75cSDP1CSl_JELvmA4iQ';
-  const range = 'Labs';
+  const range = 'Projects';
 
   const data = await getSheetData(spreadsheetId, range);
-  const labs = normalizeSheetData(data);
+  const projects = normalizeSheetData(data);
+  projects.forEach((entry) => {
+    entry.funded_by = entry.funded_by.split(',').map((item) => item.trim());
+  });
 
   return (
     <div className="grid grid-cols-2 mx-auto py-4 lg:py-5 gap-2 sm:gap-4 lg:gap-5 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 w-11/12 max-w-screen-2xl">
-      {labs.map((lab) => (
-        <LabCard key={lab.link} {...lab} />
+      {projects.map((project) => (
+        <ProjectCard key={project.link} {...project} />
       ))}
     </div>
   );
