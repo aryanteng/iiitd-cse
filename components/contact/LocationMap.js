a64a77e9d93f940cea717fe1d8b02b4b44570bef
Aryan Teng <77500690+aryanteng@users.noreply.github.com>
2024-03-09 23:21:29 +0530

    contact page design fixed (#18)

--- components/contact/LocationMap.js
+++ components/contact/LocationMap.js
@@ -1,13 +1,6 @@
 function LocationMap({ src }) {
   return (
-    <div
-      className="relative w-full"
-      style={{
-        marginLeft: '1rem',
-        marginRight: '1rem',
-        marginBottom: '1rem',
-        marginTop: '1rem',
-      }}>
+    <div className="relative w-full">
       <iframe
         src={src}
         className="absolute top-0 left-0 w-full h-96"
