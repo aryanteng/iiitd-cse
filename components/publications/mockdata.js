c13d223b10fed1c81c9b484e95e9433dfc1948cd
Yug1110 <108721211+Yug1110@users.noreply.github.com>
2024-03-09 23:21:15 +0530

    Completed publication design (#17)

---
+++ components/publications/mockdata.js
@@ -0,0 +1,340 @@
+export const publications = [
+  {
+    year: 2024,
+    title: 'Advancements in Machine Learning Algorithms',
+    description:
+      'Exploring the latest developments and trends in machine learning algorithms.',
+    authors: ['John Doe', 'Jane Doe'],
+  },
+  {
+    year: 2024,
+    title: 'Ethical Considerations in AI Research',
+    description:
+      'Analyzing the ethical implications of AI research and its impact on society.',
+    authors: ['Raman', 'Jane Doe'],
+  },
+  {
+    year: 2024,
+    title: 'The Rise of Quantum Computing',
+    description:
+      'Investigating the emergence of quantum computing technologies and their potential applications.',
+    authors: ['Alice Smith', 'Bob Johnson'],
+  },
+  {
+    year: 2024,
+    title: 'The Impact of Artificial Intelligence on Employment',
+    description:
+      'Examining the effects of AI adoption on the labor market and employment trends.',
+    authors: ['David Lee', 'Emily Brown'],
+  },
+  {
+    year: 2024,
+    title: 'Exploring AI in Healthcare: Challenges and Opportunities',
+    description:
+      'Analyzing the applications and challenges of AI technology in the healthcare industry.',
+    authors: ['Michael Wang', 'Sophia Adams'],
+  },
+  {
+    year: 2024,
+    title: 'The Future of Robotics: Trends and Innovations',
+    description:
+      'Investigating the latest trends and innovations in robotics technology.',
+    authors: ['Max Johnson', 'Ava Thompson'],
+  },
+  {
+    year: 2024,
+    title: 'Data Privacy in the Age of Artificial Intelligence',
+    description:
+      'Examining the challenges and solutions for protecting data privacy in the era of AI.',
+    authors: ['Chris Evans', 'Isabella Garcia'],
+  },
+  {
+    year: 2024,
+    title: 'The Role of AI in Climate Change Prediction',
+    description:
+      'Analyzing the use of AI models and algorithms in predicting and mitigating climate change effects.',
+    authors: ['Ryan Brown', 'Ella Martinez'],
+  },
+  {
+    year: 2024,
+    title: 'The Ethics of Autonomous Vehicles',
+    description:
+      'Investigating the ethical considerations surrounding the development and deployment of autonomous vehicles.',
+    authors: ['Jake Smith', 'Sophie Evans'],
+  },
+  {
+    year: 2024,
+    title: 'Quantum Computing Applications in Financial Markets',
+    description:
+      'Exploring the potential applications of quantum computing in financial market analysis and trading.',
+    authors: ['Kevin Clark', 'Olivia Wilson'],
+  },
+
+  //
+  {
+    year: 2024,
+    title: 'Advancements in Machine Learning Algorithms',
+    description:
+      'Exploring the latest developments and trends in machine learning algorithms.',
+    authors: ['John Doe', 'Jane Doe'],
+  },
+  {
+    year: 2024,
+    title: 'Ethical Considerations in AI Research',
+    description:
+      'Analyzing the ethical implications of AI research and its impact on society.',
+    authors: ['Raman', 'Jane Doe'],
+  },
+  {
+    year: 2024,
+    title: 'The Rise of Quantum Computing',
+    description:
+      'Investigating the emergence of quantum computing technologies and their potential applications.',
+    authors: ['Alice Smith', 'Bob Johnson'],
+  },
+  {
+    year: 2024,
+    title: 'The Impact of Artificial Intelligence on Employment',
+    description:
+      'Examining the effects of AI adoption on the labor market and employment trends.',
+    authors: ['David Lee', 'Emily Brown'],
+  },
+  {
+    year: 2023,
+    title: 'Augmented Reality in Healthcare',
+    description:
+      'Examining the use of augmented reality technology in the healthcare industry.',
+    authors: ['Emily Brown', 'David Lee'],
+  },
+  {
+    year: 2023,
+    title: 'The Future of Quantum Cryptography',
+    description:
+      'Exploring the potential applications and security implications of quantum cryptography.',
+    authors: ['Sophia Adams', 'Michael Wang'],
+  },
+  {
+    year: 2023,
+    title: 'Advancements in Natural Language Processing',
+    description:
+      'Analyzing recent advancements in natural language processing algorithms and applications.',
+    authors: ['Daniel Miller', 'Olivia Wilson'],
+  },
+  {
+    year: 2022,
+    title: 'Blockchain Beyond Cryptocurrency',
+    description:
+      'Exploring the diverse applications of blockchain technology beyond cryptocurrency.',
+    authors: ['Michael Wang', 'Sarah Adams'],
+  },
+  {
+    year: 2022,
+    title: 'The Role of Artificial Intelligence in Climate Change Mitigation',
+    description:
+      'Investigating how AI technologies can be leveraged to address climate change challenges.',
+    authors: ['Chris Evans', 'Emma Thompson'],
+  },
+  {
+    year: 2022,
+    title: 'Quantum Computing: From Theory to Practice',
+    description:
+      'Examining the practical applications and challenges of quantum computing implementation.',
+    authors: ['Ryan Brown', 'Ella Martinez'],
+  },
+  {
+    year: 2022,
+    title: 'The Future of Quantum Computing in Financial Services',
+    description:
+      'Analyzing the potential impact of quantum computing on the financial services industry.',
+    authors: ['Max Johnson', 'Ava Thompson'],
+  },
+  {
+    year: 2021,
+    title: 'The Future of Internet Privacy',
+    description:
+      'Investigating the challenges and opportunities in protecting internet privacy in the digital age.',
+    authors: ['Chris Evans', 'Emma Thompson'],
+  },
+  {
+    year: 2021,
+    title: 'Exploring the Metaverse: Opportunities and Challenges',
+    description:
+      'Examining the concept of the metaverse and its potential societal and economic impacts.',
+    authors: ['Jake Smith', 'Isabella Garcia'],
+  },
+  {
+    year: 2021,
+    title: 'The Ethics of Autonomous Vehicles',
+    description:
+      'Analyzing the ethical considerations surrounding the development and deployment of autonomous vehicles.',
+    authors: ['Andrew Brown', 'Sophie Evans'],
+  },
+  {
+    year: 2020,
+    title: 'Robotic Automation in Manufacturing',
+    description:
+      'Analyzing the adoption of robotic automation systems in the manufacturing sector.',
+    authors: ['Alex Johnson', 'Sophia Garcia'],
+  },
+  {
+    year: 2020,
+    title: 'The Future of Work: Implications of Remote Collaboration Tools',
+    description:
+      'Examining the impact of remote collaboration tools on the future of work and organizational dynamics.',
+    authors: ['Kevin Clark', 'Lily Anderson'],
+  },
+  {
+    year: 2020,
+    title: 'The Role of AI in Healthcare Diagnosis',
+    description:
+      'Investigating the applications of artificial intelligence in improving healthcare diagnosis and treatment.',
+    authors: ['Daniel Miller', 'Olivia Wilson'],
+  },
+  {
+    year: 2020,
+    title: 'Sustainable Energy Solutions for Urban Environments',
+    description:
+      'Exploring innovative sustainable energy solutions for urban areas to mitigate environmental impact.',
+    authors: ['Michael Wang', 'Sarah Adams'],
+  },
+  {
+    year: 2019,
+    title: 'Renewable Energy Technologies',
+    description:
+      'Examining the latest advancements in renewable energy technologies and their impact on sustainability.',
+    authors: ['Daniel Miller', 'Olivia Wilson'],
+  },
+  {
+    year: 2019,
+    title: 'The Impact of Artificial Intelligence on Business Operations',
+    description:
+      'Analyzing the effects of AI adoption on business operations and strategies.',
+    authors: ['Chris Evans', 'Emma Thompson'],
+  },
+  {
+    year: 2019,
+    title: 'The Future of Transportation: Electric and Autonomous Vehicles',
+    description:
+      'Investigating the potential of electric and autonomous vehicles to revolutionize transportation.',
+    authors: ['Kevin Clark', 'Lily Anderson'],
+  },
+  {
+    year: 2018,
+    title: 'Cybersecurity in a Connected World',
+    description:
+      'Investigating the evolving landscape of cybersecurity in an increasingly interconnected world.',
+    authors: ['Ryan Brown', 'Ella Martinez'],
+  },
+  {
+    year: 2018,
+    title: 'Disruptive Innovations in Fintech',
+    description:
+      'Exploring disruptive innovations and technologies in the financial technology sector.',
+    authors: ['Alex Johnson', 'Sophia Garcia'],
+  },
+  {
+    year: 2018,
+    title: 'The Impact of Social Media on Consumer Behavior',
+    description:
+      'Analyzing how social media platforms influence consumer behavior and purchasing decisions.',
+    authors: ['Jake Smith', 'Isabella Garcia'],
+  },
+  {
+    year: 2018,
+    title: 'The Future of Artificial Intelligence in Education',
+    description:
+      'Examining the potential of AI technologies to transform education and learning experiences.',
+    authors: ['Andrew Brown', 'Sophie Evans'],
+  },
+  {
+    year: 2017,
+    title: 'Big Data Analytics in Business',
+    description:
+      'Exploring the use of big data analytics to drive insights and decision-making in business.',
+    authors: ['Kevin Clark', 'Lily Anderson'],
+  },
+  {
+    year: 2017,
+    title: 'The Impact of Virtual Reality in Marketing',
+    description:
+      'Analyzing how virtual reality technology is reshaping marketing strategies and consumer engagement.',
+    authors: ['Max Johnson', 'Ava Thompson'],
+  },
+  {
+    year: 2017,
+    title: 'The Future of Renewable Energy: Solar and Wind Power',
+    description:
+      'Investigating the advancements and challenges in solar and wind power technologies.',
+    authors: ['Daniel Miller', 'Olivia Wilson'],
+  },
+  {
+    year: 2016,
+    title: 'Future Trends in Mobile Technology',
+    description:
+      'Analyzing the potential future trends and innovations in mobile technology.',
+    authors: ['Max Johnson', 'Ava Thompson'],
+  },
+  {
+    year: 2016,
+    title: 'The Role of Artificial Intelligence in Personal Assistants',
+    description:
+      'Examining the evolution and potential applications of AI in personal assistant technologies.',
+    authors: ['Chris Evans', 'Emma Thompson'],
+  },
+  {
+    year: 2016,
+    title: 'The Impact of 3D Printing on Manufacturing',
+    description:
+      'Investigating the applications and benefits of 3D printing technology in the manufacturing industry.',
+    authors: ['Alex Johnson', 'Sophia Garcia'],
+  },
+  {
+    year: 2015,
+    title: 'The Impact of Social Media on Society',
+    description:
+      'Examining the social, cultural, and economic impact of social media platforms on society.',
+    authors: ['Jake Smith', 'Isabella Garcia'],
+  },
+  {
+    year: 2015,
+    title: 'The Future of Wearable Technology',
+    description:
+      'Exploring the potential applications and challenges of wearable technology devices.',
+    authors: ['Andrew Brown', 'Sophie Evans'],
+  },
+  {
+    year: 2015,
+    title: 'The Role of Artificial Intelligence in Customer Service',
+    description:
+      'Analyzing how AI technologies are transforming customer service interactions and experiences.',
+    authors: ['Kevin Clark', 'Lily Anderson'],
+  },
+  {
+    year: 2014,
+    title: 'Space Exploration Technologies',
+    description:
+      'Investigating the latest advancements in space exploration technologies and missions.',
+    authors: ['Andrew Brown', 'Sophie Evans'],
+  },
+  {
+    year: 2014,
+    title: 'The Future of Energy Storage: Batteries and Beyond',
+    description:
+      'Examining innovative energy storage solutions beyond traditional battery technologies.',
+    authors: ['Daniel Miller', 'Olivia Wilson'],
+  },
+  {
+    year: 2014,
+    title: 'The Impact of Internet of Things on Smart Cities',
+    description:
+      'Analyzing how IoT technologies are transforming urban infrastructure and services.',
+    authors: ['Chris Evans', 'Emma Thompson'],
+  },
+  {
+    year: 2014,
+    title: 'The Rise of Autonomous Drones',
+    description:
+      'Investigating the applications and challenges of autonomous drone technologies.',
+    authors: ['Max Johnson', 'Ava Thompson'],
+  },
+];
