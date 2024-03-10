155ecc2eb5f172c96e8ecf17ab36de50db59c419
Pritish Poswal <77208293+pritishposwal@users.noreply.github.com>
2024-03-11 00:49:44 +0530

    Integrated backend for faculty page

--- app/people/faculty/page.js
+++ app/people/faculty/page.js
@@ -1,10 +1,17 @@
+import { getSheetData } from '@/app/api/sheets';
 import FacultyCard from '@/components/people/faculty/FacultyCard';
+import { normalizeSheetData } from '@/utils/normaliseSheetData';
 
-export default function Faculty() {
+export default async function Faculty() {
+  const spreadsheetId = '1cYPbswNr4ZvvQhzkfIOaM9Q75cSDP1CSl_JELvmA4iQ';
+  const range = 'Faculty';
+
+  const data = await getSheetData(spreadsheetId, range);
+  const faculty = normalizeSheetData(data);
   return (
     <div className="grid grid-cols-2 mx-auto py-4 lg:py-5 gap-2 sm:gap-4 lg:gap-5 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 w-11/12 max-w-screen-2xl">
-      {Array.from({ length: 40 }).map((faculty, index) => (
-        <FacultyCard key={index} />
+      {faculty.map((prof, index) => (
+        <FacultyCard key={index} {...prof} />
       ))}
     </div>
   );
--- components/people/faculty/FacultyCard.js
+++ components/people/faculty/FacultyCard.js
@@ -2,11 +2,18 @@
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GoogleIcon from '@mui/icons-material/Google';
 
-export default function FacultyCard() {
+export default function FacultyCard({
+  'faculty image': image,
+  name,
+  designation,
+  education,
+  'google scholar': googleScholar,
+  linkedin,
+}) {
   return (
     <div className="w-full shadow-md hover:shadow-xl bg-white border-solid cursor-pointer duration-500 overflow-hidden">
       <img
-        src="/images/people/faculty.png"
+        src={image || '/images/people/faculty.png'}
         className="object-cover border-b-1 w-full relative aspect-w-16 aspect-h-9"
         loading="lazy"
         alt="Faculty"
@@ -14,19 +21,23 @@ export default function FacultyCard() {
       <div className="flex flex-col gap-2 px-2 py-4">
         <div className="flex items-center justify-between">
           <div className="text-primary-main font-semibold body-normal">
-            A V Subramanyam
+            {name}
           </div>
           <div className="flex gap-1">
-            <GoogleIcon className="text-primary-main body-normal" />
-            <LinkedInIcon className="text-primary-main body-normal" />
+            {googleScholar && (
+              <a href={googleScholar} target="_blank" rel="noopener noreferrer">
+                <GoogleIcon className="text-primary-main body-normal" />
+              </a>
+            )}
+            {linkedin && (
+              <a href={linkedin} target="_blank" rel="noopener noreferrer">
+                <LinkedInIcon className="text-primary-main body-normal" />
+              </a>
+            )}
           </div>
         </div>
-        <div className="body-small font-semibold">
-          Associate Professor (ECE, CSE)
-        </div>
-        <div className="body-xsmall">
-          PhD (2012), Nanyang Technological University, Singapore
-        </div>
+        <div className="body-small font-semibold">{designation}</div>
+        <div className="body-xsmall">{education}</div>
       </div>
     </div>
   );
