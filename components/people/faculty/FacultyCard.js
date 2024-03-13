446e82624448bd662e6c2b571fb55e72e8314820
Pritish Poswal <77208293+pritishposwal@users.noreply.github.com>
2024-03-13 11:59:27 +0530

    Integrated backend for faculty page (#21)
    * Integrated backend for faculty page
    
    * fixed overflow issue with professors who are on leave
    
    * minor css changes
    
    ---------
    
    Co-authored-by: aryanteng <aryanteng892@gmail.com>

--- components/people/faculty/FacultyCard.js
+++ components/people/faculty/FacultyCard.js
@@ -2,31 +2,57 @@
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GoogleIcon from '@mui/icons-material/Google';
 
-export default function FacultyCard() {
+export default function FacultyCard({
+  'faculty image': image,
+  name,
+  designation,
+  education,
+  'google scholar': googleScholar,
+  linkedin,
+}) {
+  // Check if the name includes "(On Leave)"
+  const onLeave = name.includes('On Leave');
+  // variable to store if faculty is on leave
+  const status = onLeave ? 'On Leave' : '';
+  // If faculty is on leave, remove "(On Leave)" from the name
+  if (onLeave) {
+    name = name.replace('(On Leave)', '');
+  }
   return (
-    <div className="w-full shadow-md hover:shadow-xl bg-white border-solid cursor-pointer duration-500 overflow-hidden">
-      <img
-        src="/images/people/faculty.png"
-        className="object-cover border-b-1 w-full relative aspect-w-16 aspect-h-9"
-        loading="lazy"
-        alt="Faculty"
-      />
+    <div className="w-full shadow-md bg-white border-solid overflow-hidden">
+      <div className="relative h-auto">
+        <img
+          src={image || '/images/people/faculty.png'}
+          className="object-cover border-b-1 w-full relative aspect-w-16 aspect-h-9"
+          loading="lazy"
+          alt="Faculty"
+        />
+        {onLeave ? (
+          <div className="absolute cursor-default bottom-2 right-2 bg-primary-main text-white py-1 px-3 font-semibold rounded-xl text-sm">
+            {status}
+          </div>
+        ) : null}
+      </div>
       <div className="flex flex-col gap-2 px-2 py-4">
         <div className="flex items-center justify-between">
           <div className="text-primary-main font-semibold body-normal">
-            A V Subramanyam
+            {name}
           </div>
           <div className="flex gap-1">
-            <GoogleIcon className="text-primary-main body-normal" />
-            <LinkedInIcon className="text-primary-main body-normal" />
+            {googleScholar && (
+              <a href={googleScholar} target="_blank" rel="noopener noreferrer">
+                <GoogleIcon className="text-primary-main body-normal" />
+              </a>
+            )}
+            {linkedin && (
+              <a href={linkedin} target="_blank" rel="noopener noreferrer">
+                <LinkedInIcon className="text-primary-main body-normal" />
+              </a>
+            )}
           </div>
         </div>
-        <div className="body-small font-semibold">
-          Associate Professor (ECE, CSE)
-        </div>
-        <div className="body-xsmall">
-          PhD (2012), Nanyang Technological University, Singapore
-        </div>
+        <div className="body-small font-semibold">{designation}</div>
+        <div className="body-xsmall">{education}</div>
       </div>
     </div>
   );
