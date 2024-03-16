384584c9e92a255554b7e789bfda2a480fb542c5
ritsiiitd <ritick20460@iiitd.ac.in>
2024-03-16 15:45:59 +0530

    fixing swiper carousel

--- components/admissions/phd/CustomCarousel.js
+++ components/admissions/phd/CustomCarousel.js
@@ -1,74 +1,91 @@
 'use client';
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import {
+  ArrowBackIosNewRounded,
+  ArrowForwardIosRounded,
+} from '@mui/icons-material';
 
 export default function App() {
   return (
-    <Swiper
-      spaceBetween={30}
-      autoHeight={true}
-      centeredSlides={true}
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
-      }}
-      pagination={{
-        clickable: true,
-      }}
-      navigation={true}
-      modules={[Autoplay, Pagination, Navigation]}
-      className="mySwiper">
-      <SwiperSlide>
-        <div className=" h-80 flex justify-center items-center">
-          <img
-            src="/images/admissions/phd/carousel2.jpg"
-            alt="..."
-            className="object-cover"
-          />
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="h-80 flex justify-center items-center">
-          <img
-            src="/images/admissions/phd/carousel1.jpg"
-            alt="..."
-            className="object-cover"
-          />
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="h-80 flex justify-center items-center">
-          <img
-            src="/images/admissions/phd/carousel3.jpg"
-            alt="..."
-            className="object-cover"
-          />
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="h-80 flex justify-center items-center">
-          <img
-            src="/images/admissions/phd/carousel4.jpg"
-            alt="..."
-            className="object-cover"
-          />
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="h-80 flex justify-center items-center">
-          <img
-            src="/images/admissions/phd/carousel5.jpg"
-            alt="..."
-            className="object-cover"
-          />
-        </div>
-      </SwiperSlide>
-    </Swiper>
+    <div className="relative">
+      <Swiper
+        spaceBetween={30}
+        autoHeight={true}
+        centeredSlides={true}
+        loop={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={{ nextEl: '.arrow-left', prevEl: '.arrow-right' }}
+        modules={[Autoplay, Navigation]}
+        className="mySwiper">
+        <SwiperSlide>
+          <div className=" h-80 flex justify-center items-center">
+            <img
+              src="/images/admissions/phd/carousel2.jpg"
+              alt="..."
+              className="object-cover"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="h-80 flex justify-center items-center">
+            <img
+              src="/images/admissions/phd/carousel1.jpg"
+              alt="..."
+              className="object-cover"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="h-80 flex justify-center items-center">
+            <img
+              src="/images/admissions/phd/carousel3.jpg"
+              alt="..."
+              className="object-cover"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="h-80 flex justify-center items-center">
+            <img
+              src="/images/admissions/phd/carousel4.jpg"
+              alt="..."
+              className="object-cover"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="h-80 flex justify-center items-center">
+            <img
+              src="/images/admissions/phd/carousel5.jpg"
+              alt="..."
+              className="object-cover"
+            />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+
+      <div className="absolute left-0 z-10 top-1/2 transform ml-2 px-3 py-2 rounded-full bg-primary-dark opacity-60 hover:opacity-100 -translate-y-1/2">
+        <button className="arrow-right arrow">
+          <ArrowBackIosNewRounded className="text-white" />
+        </button>
+      </div>
+      <div className="absolute right-0 z-10 top-1/2 px-3 py-2 mr-2 rounded-full bg-primary-dark transform opacity-60 hover:opacity-100 -translate-y-1/2">
+        <button className="arrow-left arrow ">
+          <ArrowForwardIosRounded className="text-white" />
+        </button>
+      </div>
+    </div>
   );
 }
