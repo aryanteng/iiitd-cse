008a694b90a26ecc8b9b91a6a62db8b1e12ef952
Pritish Poswal <77208293+pritishposwal@users.noreply.github.com>
2024-04-13 08:05:18 +0530

    adding tabs to projects (#29)

--- app/research/projects/page.js
+++ app/research/projects/page.js
@@ -1,6 +1,7 @@
 import { getSheetData } from '@/app/api/sheets';
 import ProjectCard from '@/components/research/projects/ProjectCard';
 import { normalizeSheetData } from '@/utils/normaliseSheetData';
+import InteractiveProjects from '@/components/research/projects/InteractiveProjects';
 
 export default async function Projects() {
   const spreadsheetId = '1cYPbswNr4ZvvQhzkfIOaM9Q75cSDP1CSl_JELvmA4iQ';
@@ -11,12 +12,5 @@ export default async function Projects() {
   projects.forEach((entry) => {
     entry.funded_by = entry.funded_by.split(',').map((item) => item.trim());
   });
-
-  return (
-    <div className="grid grid-cols-2 mx-auto py-4 lg:py-5 gap-2 sm:gap-4 lg:gap-5 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 w-11/12 max-w-screen-2xl">
-      {projects.map((project) => (
-        <ProjectCard key={project.title} {...project} />
-      ))}
-    </div>
-  );
+  return <InteractiveProjects projects={projects} />;
 }
