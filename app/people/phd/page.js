8c81b9ac6314b43c76cc1a4dc74c7a4f2dadbf88
Aryan Gupta <96881807+aryanGupta-09@users.noreply.github.com>
2024-06-14 18:41:47 +0530

    added search to phd page and some design changes

--- app/people/phd/page.js
+++ app/people/phd/page.js
@@ -1,18 +1,20 @@
 import { getSheetData } from '@/app/api/sheets';
-import PhdCard from '@/components/people/phd/PhdCard';
 import { normalizeSheetData } from '@/utils/normaliseSheetData';
+import PhdList from '@/components/people/phd/PhdList';
 
 export default async function Phd() {
   const spreadsheetId = '1cYPbswNr4ZvvQhzkfIOaM9Q75cSDP1CSl_JELvmA4iQ';
   const range = 'Phd Students';
 
   const data = await getSheetData(spreadsheetId, range);
-  const phd = normalizeSheetData(data);
+  const phdStudents = normalizeSheetData(data);
+  phdStudents.forEach((entry) => {
+    entry.funded_by = entry.funded_by.split(',').map((item) => item.trim());
+  });
+
   return (
-    <div className="grid grid-cols-2 mx-auto py-4 lg:py-5 gap-2 sm:gap-4 lg:gap-5 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 w-11/12 max-w-screen-2xl">
-      {phd.map((prof) => (
-        <PhdCard key={prof.email} {...prof} />
-      ))}
-    </div>
+    <main className="padding-layout-2 px-2 md:px-10">
+      <PhdList phdStudents={phdStudents} />
+    </main>
   );
 }
