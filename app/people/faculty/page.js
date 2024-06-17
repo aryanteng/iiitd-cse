af5dcbd6e6a6e5da2d70650e26248b5d3788c654
Aryan Gupta <96881807+aryanGupta-09@users.noreply.github.com>
2024-06-17 13:55:21 +0530

    updated faculty page (#43)

--- app/people/faculty/page.js
+++ app/people/faculty/page.js
@@ -9,7 +9,7 @@ export default async function Faculty() {
   const data = await getSheetData(spreadsheetId, range);
   const faculty = normalizeSheetData(data);
   return (
-    <div className="grid grid-cols-2 mx-auto py-4 lg:py-5 gap-2 sm:gap-4 lg:gap-5 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 w-11/12 max-w-screen-2xl">
+    <div className="grid gap-2 py-2 mx-auto grid-cols-2 sm:gap-4 sm:py-4 sm:grid-cols-3 md:grid-cols-4 lg:gap-5 lg:py-5 lg:grid-cols-5 2xl:grid-cols-7 w-11/12 max-w-screen-2xl">
       {faculty.map((prof, index) => (
         <FacultyCard key={faculty.linkedin} {...prof} />
       ))}
