a207895a005bc11047d3cb42853baa9a230f1073
Aryan Gupta <96881807+aryanGupta-09@users.noreply.github.com>
2024-06-20 20:05:17 +0530

    combined mission and vision, added recent publications carousel

--- app/page.js
+++ app/page.js
@@ -1,24 +1,29 @@
 import Banner from '@/components/landing/Banner';
 import MissionAndVision from '@/components/landing/MissionAndVision';
+import RecentPublications from '@/components/landing/RecentPublications';
+import { normalizeSheetData } from '@/utils/normaliseSheetData';
+import { getSheetData } from './api/sheets';
 
 export default async function Home() {
+  const spreadsheetId = '1cYPbswNr4ZvvQhzkfIOaM9Q75cSDP1CSl_JELvmA4iQ';
+  const range = 'Publications';
+
+  const data = await getSheetData(spreadsheetId, range);
+  const publications = normalizeSheetData(data);
+
+  const dblpIds = [];
+  publications.forEach((pub) => {
+    if (pub.dblp) {
+      dblpIds.push(pub.dblp);
+    }
+  });
+
   return (
     <main className="flex flex-col items-center justify-center gap-4">
       <div className="[&>*:nth-child(even)]:bg-gg w-full">
         <Banner />
-        <MissionAndVision
-          name={'Mission'}
-          description={
-            'The mission of the department is to establish itself as a world-class entity by conducting advanced research and development in diverse areas of computer science, as well as applying computer and software technologies across various domains. Additionally, the department is committed to training and educating engineers of exceptional caliber at both undergraduate and postgraduate levels, with the aim of molding them into innovators and creators of new products.'
-          }
-        />
-        <MissionAndVision
-          name={'Vision'}
-          description={
-            'The vision of the department is to evolve into a globally respected, world-class entity, renowned for its research capabilities. It aspires to have some research groups recognized as global and national leaders. The department is committed to nurturing thriving undergraduate and postgraduate programs, ensuring its relevance to society. It aims to maintain a global presence and establish strong connections with industry, reinforcing its position as an industry-facing and socially relevant institution.'
-          }
-          isReverse={true}
-        />
+        <MissionAndVision />
+        <RecentPublications dblpIds={dblpIds} />
       </div>
     </main>
   );
--- components/landing/MissionAndVision.js
+++ components/landing/MissionAndVision.js
@@ -1,38 +1,34 @@
 /* eslint-disable @next/next/no-img-element */
-import PropTypes from 'prop-types';
 
-export default function MissionAndVision({ name, description, isReverse }) {
+export default function MissionAndVision() {
   return (
     <div>
-      <div
-        className={
-          'banner-layout flex md:flex-row flex-col justify-between items-center md:gap-10 gap-5' +
-          (isReverse ? ' md:flex-row-reverse' : '')
-        }>
+      <div className="banner-layout flex md:flex-row flex-col justify-between items-center md:gap-10 gap-5">
         <img
-          src={
-            isReverse
-              ? 'https://upload.wikimedia.org/wikipedia/commons/2/20/Iiitdelhi-building.jpg'
-              : 'https://iiitd.ac.in/sites/default/files/images/iiitdrndblock.jpeg'
-          }
-          alt="Vision&Mission"
+          src="https://iiitd.ac.in/sites/default/files/images/iiitdrndblock.jpeg"
+          alt="Mission&Vision"
           className="flex self-center w-full md:w-1/2 aspect-[5/3] relative"
         />
         <div className="flex flex-col gap-4">
-          <div className="body-2xlarge font-semibold">{name}</div>
-          <div className="body-xsmall">{description}</div>
+          <div className="body-2xlarge font-semibold">Mission and Vision</div>
+          <div className="body-xsmall">
+            The department aspires to be a world-class entity dedicated to
+            carrying out advanced research and development in various areas of
+            computer science, as well as the application of computer and
+            software technologies in different domain areas. We aim to train and
+            educate engineers of outstanding ability at both undergraduate and
+            postgraduate levels, fostering innovators and new product creators.
+            <br />
+            <br />
+            Our goal is to be globally respected for our research capability,
+            with certain research groups recognized as leaders both
+            internationally and within the country. We strive to maintain
+            thriving undergraduate and postgraduate programs that are socially
+            relevant, globally connected, and closely aligned with industry
+            needs.
+          </div>
         </div>
       </div>
     </div>
   );
 }
-
-MissionAndVision.propTypes = {
-  name: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  isReverse: PropTypes.bool,
-};
-
-MissionAndVision.defaultProps = {
-  isReverse: false,
-};
