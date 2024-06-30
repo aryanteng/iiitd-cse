f427f3346e93248d0aeced41e141d8d8449f82a0
Aryan Gupta <96881807+aryanGupta-09@users.noreply.github.com>
2024-06-30 21:20:38 +0530

    completed table filtering (#52)
    * corrected filtering in catalog, added filter in btech
    
    * fixed home tab on collapsed list

--- components/layout/header/CustomDrawer.js
+++ components/layout/header/CustomDrawer.js
@@ -21,54 +21,64 @@ export default function CustomDrawer({ open, anchor, toggleDrawer }) {
     <Drawer anchor={anchor} open={open} onClose={toggleDrawer}>
       <Box sx={{ width: 250 }} role="presentation" onKeyDown={toggleDrawer}>
         <List>
-          {links.map((link) => (
-            <ListItem
-              key={link.name}
-              disablePadding
-              className="w-full flex flex-col">
-              <ListItemButton
-                onClick={() => handleClick(link.name)}
-                className="w-full">
-                <ListItemText primary={link.name} />
-                {openStates[link.name] ? (
-                  <ExpandLess className="text-primary-main" />
-                ) : (
-                  <ExpandMore className="text-primary-main" />
-                )}
-              </ListItemButton>
-              <Collapse
-                className="w-full"
-                in={openStates[link.name]}
-                timeout="auto"
-                unmountOnExit>
-                <List component="div" disablePadding>
-                  {link.menuItems.map((item, itemIndex) => (
-                    <Fragment key={itemIndex}>
-                      {item.link.includes('https') ? (
-                        <a
-                          href={item.link}
-                          target="_blank"
-                          rel="noopener noreferrer">
-                          <ListItemButton
-                            onClick={() => handleClick(item.name)}>
-                            <ListItemText primary={item.name} />
-                          </ListItemButton>
-                        </a>
-                      ) : (
-                        <Link href={item.link}>
-                          <ListItemButton
-                            onClick={() => handleClick(item.name)}>
-                            <ListItemText primary={item.name} />
-                          </ListItemButton>
-                        </Link>
-                      )}
-                    </Fragment>
-                  ))}
-                </List>
-                <Divider />
-              </Collapse>
-            </ListItem>
-          ))}
+          {links.map((link) =>
+            link.name === 'Home' ? (
+              <Link key={link.name} href="/">
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemText primary={link.name} />
+                  </ListItemButton>
+                </ListItem>
+              </Link>
+            ) : (
+              <ListItem
+                key={link.name}
+                disablePadding
+                className="w-full flex flex-col">
+                <ListItemButton
+                  onClick={() => handleClick(link.name)}
+                  className="w-full">
+                  <ListItemText primary={link.name} />
+                  {openStates[link.name] ? (
+                    <ExpandLess className="text-primary-main" />
+                  ) : (
+                    <ExpandMore className="text-primary-main" />
+                  )}
+                </ListItemButton>
+                <Collapse
+                  className="w-full"
+                  in={openStates[link.name]}
+                  timeout="auto"
+                  unmountOnExit>
+                  <List component="div" disablePadding>
+                    {link.menuItems.map((item, itemIndex) => (
+                      <Fragment key={itemIndex}>
+                        {item.link.includes('https') ? (
+                          <a
+                            href={item.link}
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            <ListItemButton
+                              onClick={() => handleClick(item.name)}>
+                              <ListItemText primary={item.name} />
+                            </ListItemButton>
+                          </a>
+                        ) : (
+                          <Link href={item.link}>
+                            <ListItemButton
+                              onClick={() => handleClick(item.name)}>
+                              <ListItemText primary={item.name} />
+                            </ListItemButton>
+                          </Link>
+                        )}
+                      </Fragment>
+                    ))}
+                  </List>
+                  <Divider />
+                </Collapse>
+              </ListItem>
+            ),
+          )}
         </List>
       </Box>
     </Drawer>
